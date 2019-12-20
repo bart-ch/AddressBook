1b228b74bfb00c3c6351e0955f18b800f36ea87a
Bartosz Chodyła <bartoszchodyla@gmail.com>
2019-12-20 22:05:15 +0100

    Add registered user to vector

--- Uzytkownik.h
+++ Uzytkownik.h
@@ -4,6 +4,17 @@ using namespace std;
 
 class Uzytkownik
 {
-        int id;
-    string login, haslo;
+    int id;
+    string login;
+    string haslo;
+
+public:
+    void ustawId(int noweId);
+    void ustawLogin(string nowyLogin);
+    void ustawHaslo(string noweHaslo);
+
+    int pobierzId();
+    string pobierzLogin();
+    string pobierzHaslo();
+
 };
--- main.cpp
+++ main.cpp
@@ -7,5 +7,8 @@ int main()
 {
 
     KsiazkaAdresowa ksiazka;
+    ksiazka.rejestracjaUzytkownika();
+    ksiazka.rejestracjaUzytkownika();
+    ksiazka.wypiszWszystkichUzytkownikow();
     return 0;
 }
