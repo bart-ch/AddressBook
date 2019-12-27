2c0289a575e9d44604a852a592df060665063a72
Bartosz Chodyła <bartoszchodyla@gmail.com>
2019-12-27 20:49:42 +0100

    Added menu to main

--- main.cpp
+++ main.cpp
@@ -14,6 +14,66 @@ int main()
     ksiazka.logowanieUzytkownika();
     ksiazka.wypiszWszystkichAdresatow();
 
+    char wybor;
+
+    while (true)
+    {
+        if (UzytkownikMenedzer::pobierzIdZalogowanegoUzytkownika() == 0)
+        {
+            wybor = wybierzOpcjeZMenuGlownego();
+
+            switch (wybor)
+            {
+            case '1':
+                ksiazka.rejestracjaUzytkownika();
+                break;
+            case '2':
+                ksiazka.logowanieUzytkownika();
+                break;
+            case '9':
+                exit(0);
+                break;
+            default:
+                cout << endl << "Nie ma takiej opcji w menu." << endl << endl;
+                system("pause");
+                break;
+            }
+        }
+        else
+        {
+
+            wybor = wybierzOpcjeZMenuUzytkownika();
+
+            switch (wybor)
+            {
+            case '1':
+                ksiazka.dodajAdresata()
+                break;
+            case '2':
+            //    wyszukajAdresatowPoImieniu(adresaci);
+                break;
+            case '3':
+        //        wyszukajAdresatowPoNazwisku(adresaci);
+                break;
+            case '4':
+                ksiazka.wypiszWszystkichAdresatow();
+                break;
+            case '5':
+            //    idUsunietegoAdresata = usunAdresata(adresaci);
+           //     idOstatniegoAdresata = podajIdOstatniegoAdresataPoUsunieciuWybranegoAdresata(idUsunietegoAdresata, idOstatniegoAdresata);
+                break;
+            case '6':
+           //     edytujAdresata(adresaci);
+                break;
+            case '7':
+                ksiazka.zmianaHaslaZalogowanegoUzytkownika();
+                break;
+            case '8':
+                ksiazka.wyloguj();
+                break;
+            }
+        }
+    }
 
     return 0;
 }
